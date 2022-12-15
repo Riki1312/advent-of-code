2ad8d9dcf3fbfe1d7b7c5f33599baa78080d109c
Riccardo <riki.brero@gmail.com>
2022-12-15 17:48:13 +0100

    2022

--- dart-2022/day1/a.dart
+++ dart-2022/day1/a.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 void main() {
-  print('Hello, World!');
+  final input = File("test.txt").readAsStringSync().trim();
+
+  final result = input
+      .split("\n\r")
+      .map((s) => s.split("\n"))
+      .map((l) => l.where((e) => e.trim().isNotEmpty).map((e) => int.parse(e)))
+      .map((l) => l.reduce((a, b) => a + b))
+      .toList()
+    ..sort();
+
+  print(result.last);
 }
