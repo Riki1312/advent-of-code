45379b99aa100da7a60b3b8557f83aeb3c74bb33
Riccardo <riki.brero@gmail.com>
2023-01-27 19:44:02 +0100

    Rust 2022

---
+++ rust-2022/day1/target/debug/deps/day1-9a7b0d4914ce2352.d
@@ -0,0 +1,5 @@
+D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-9a7b0d4914ce2352.rmeta: src\main.rs
+
+D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-9a7b0d4914ce2352.d: src\main.rs
+
+src\main.rs:
