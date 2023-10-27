745e051beb03d71bb5ba184de199f8b66b343c2b
Riccardo Brero <riccardo.brero@satispay.com>
2023-10-27 21:25:32 +0200

    .gitignore

--- rust-2022/day1/target/debug/deps/day1-9a7b0d4914ce2352.d
+++
@@ -1,5 +0,0 @@
-D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-9a7b0d4914ce2352.rmeta: src\main.rs
-
-D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-9a7b0d4914ce2352.d: src\main.rs
-
-src\main.rs:
--- rust-2022/day1/target/debug/deps/day1-a7dfcd2de88b8a3b.d
+++
@@ -1,5 +0,0 @@
-D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-a7dfcd2de88b8a3b.rmeta: src\main.rs
-
-D:\Documenti\GitHub\advent-of-code\rust-2022\day1\target\debug\deps\day1-a7dfcd2de88b8a3b.d: src\main.rs
-
-src\main.rs:
