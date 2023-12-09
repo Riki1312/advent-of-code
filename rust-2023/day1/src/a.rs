438164c07369f06244706fc5df404ef17210f034
Riccardo Brero <riccardo.brero@satispay.com>
2023-12-09 22:09:18 +0100

    rust 2023

--- rust-2023/day1/src/a.rs
+++ rust-2023/day1/src/a.rs
@@ -1,7 +1,23 @@
 use utils::ProcessResult;
 
 pub fn process(input: &str) -> ProcessResult {
-    Ok("Hello form day1-a!".to_string())
+    let result = input.lines()
+        .map(process_line)
+        .sum::<i32>();
+
+    return Ok(result.to_string());
+}
+
+fn process_line(line: &str) -> i32 {
+    let first = line.chars()
+        .find(char::is_ascii_digit)
+        .expect("first digit");
+    let last = line.chars()
+        .rev()
+        .find(char::is_ascii_digit)
+        .expect("last digit");
+
+    format!("{}{}", first, last).parse::<i32>().expect("must be a number")
 }
 
 #[cfg(test)]
@@ -10,8 +26,11 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "";
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
         let result = process(input).unwrap();
-        assert_eq!(result, "Hello form day1-a!");
+        assert_eq!(result, "142");
     }
 }
--- rust-2023/day1/src/b.rs
+++ rust-2023/day1/src/b.rs
@@ -1,7 +1,42 @@
 use utils::ProcessResult;
 
 pub fn process(input: &str) -> ProcessResult {
-    Ok("Hello form day1-b!".to_string())
+    let result = input.lines()
+        .map(process_line)
+        .sum::<i32>();
+
+    return Ok(result.to_string());
+}
+
+fn process_line(line: &str) -> i32 {
+    let first = (0..line.len())
+        .find_map(|i| { get_number(line, i) })
+        .expect("first digit");
+
+    let last = (0..line.len())
+        .rev()
+        .find_map(|i| { get_number(line, i) })
+        .expect("last digit");
+
+    format!("{}{}", first, last).parse::<i32>().expect("must be a number")
+}
+
+fn get_number(line: &str, idx: usize) -> Option<char> {
+    line.chars().nth(idx)
+        .filter(char::is_ascii_digit)
+        .or_else(|| { find_number(line, idx) })
+}
+
+fn find_number(line: &str, idx: usize) -> Option<char> {
+    ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].iter()
+        .enumerate()
+        .find(|(i, e)| {
+            line[idx..].starts_with(e.chars().as_str())
+        })
+        .map(|(i, _)| {
+            (i + 1).to_string().chars().nth(0)
+        })
+        .flatten()
 }
 
 #[cfg(test)]
@@ -10,8 +45,14 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "";
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
         let result = process(input).unwrap();
-        assert_eq!(result, "Hello form day1-b!");
+        assert_eq!(result, "281");
     }
 }
